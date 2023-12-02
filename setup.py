2d9cc54c3052440de0ae8623b1ebb637b7df59c9
zer0 <erik172dev@gmail.com>
2023-12-01 23:32:27 -0500

    Update setup.py with version and additional information

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
 
+VERSION = '0.0.1'
+
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
     name='torombolo',
-    version='0.1',
+    version=VERSION,
     packages=find_packages(),
     install_requires=requirements,
     author='AI Group Uniandes',
     description='Machine Learning AI library for Python',
+    long_description=open('README.md').read(),
+    url='https://github.com/Computer-Society-Uniandes/Torombolo_Library',
+    license='MIT',
+    python_requires='>=3.6'
 )
